cdddca1a24b5aca63dc3c55455adf7ab33817055
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2024-02-19 23:55:18 -0300

    :sparkles: feat: add sessionId to transactions with cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -22,7 +22,7 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { transaction }
   })
 
-  app.get('/summary', async (request) => {
+  app.get('/summary', async () => {
     const summary = await knex('transactions').sum('amount', { as: 'amount' })
 
     return { summary }
@@ -38,7 +38,17 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body,
     )
 
-    await knex('transaction').insert({
+    let sessionId = request.cookies.sessionId
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
