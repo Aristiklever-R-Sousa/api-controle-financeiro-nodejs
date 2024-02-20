de752c2a83dd3bc769bd4c66f26372b946117215
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2024-02-19 22:53:13 -0300

    :sparkles: feat: add store transactions route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
-import crypto from 'node:crypto'
 import fastify from 'fastify'
-import { knex } from './database'
+
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transaction de Teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
+app.register(transactionRoutes, {
+  prefix: 'transactions',
 })
 
 app
